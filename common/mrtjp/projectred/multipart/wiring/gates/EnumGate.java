909b6367d1f69b19080cc93d7cef4b50de230435
Taelnia <shotchkiss+github@gmail.com>
2013-07-23 15:09:07 -0400

    Revert "add lightcount to EnumGate for use BlockGate.getLightLevel"
    This reverts commit c88efdb50694d4233927b57ea7a243c347f24f97.

--- common/mrtjp/projectred/multipart/wiring/gates/EnumGate.java
+++ common/mrtjp/projectred/multipart/wiring/gates/EnumGate.java
@@ -12,46 +12,44 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public enum EnumGate {
-	AND("AND gate", 4, GateLogic.AND.class, GateRenderBridge.AND.class),
-	OR("OR gate", 2, GateLogic.OR.class, GateRenderBridge.OR.class),
-	NOT("NOT gate", 1, GateLogic.NOT.class, GateRenderBridge.NOT.class),
-	RSLATCH("RS-Latch", 2, GateLogic.RSLatch.class, GateRenderBridge.RSLatch.class),
-	TOGGLE("T-FlipFlop", 2, GateLogic.ToggleLatch.class, GateRenderBridge.ToggleLatch.class),
-	NOR("NOR gate", 1, GateLogic.NOR.class, GateRenderBridge.NOR.class),
-	NAND("NAND gate", 3, GateLogic.NAND.class, GateRenderBridge.NAND.class),
-	XOR("XOR gate", 3, GateLogic.XOR.class, GateRenderBridge.XOR.class),
-	XNOR("XNOR gate", 4, GateLogic.XNOR.class, GateRenderBridge.XNOR.class),
-	Buffer("Buffer gate", 2, GateLogic.Buffer.class, GateRenderBridge.Buffer.class),
-	Multiplexer("Multiplexer", 4, GateLogic.Multiplexer.class, GateRenderBridge.Multiplexer.class),
-	Repeater("Repeater", 2, GateLogic.Repeater.class, GateRenderBridge.Repeater.class),
-	Timer("Timer", 2, GateLogic.Timer.class, GateRenderBridge.Timer.class),
-	Counter("Counter", 3, GateLogic.Counter.class, GateRenderBridge.Counter.class),
-	Sequencer("Sequencer", 5, GateLogic.Sequencer.class, GateRenderBridge.Sequencer.class),
-	PulseFormer("Pulse Former", 3, GateLogic.PulseFormer.class, GateRenderBridge.PulseFormer.class),
-	Randomizer("Randomizer", 0, GateLogic.Randomizer.class, GateRenderBridge.Randomizer.class),
-	StateCell("State Cell", 2, GateLogic.StateCell.class, GateRenderBridge.StateCell.class),
-	Synchronizer("Synchronizer", 1, GateLogic.Synchronizer.class, GateRenderBridge.Synchronizer.class),
-	DLatch("D-Latch", 0, GateLogic.DLatch.class, GateRenderBridge.DLatch.class),
-	DFlop("D-FlipFlop", 0, GateLogic.DFlop.class, GateRenderBridge.DFlop.class),
-	BundledLatch("Bundled Latch", 0, GateLogic.BundledLatch.class, GateRenderBridge.BundledLatch.class),
-	BundledRelay("Bundled Relay", 0, GateLogic.BundledRelay.class, GateRenderBridge.BundledRelay.class),
-	BundledMultiplexer("Bundled Multiplexer", 0, GateLogic.BundledMultiplexer.class, GateRenderBridge.BundledMultiplexer.class),
-	LightSensor("Light Sensor", 0, GateLogic.LightSensor.class, GateRenderBridge.LightSensor.class),
-	RainSensor("Rain Sensor", 0, GateLogic.RainSensor.class, GateRenderBridge.RainSensor.class)
+	AND("AND gate", GateLogic.AND.class, GateRenderBridge.AND.class),
+	OR("OR gate", GateLogic.OR.class, GateRenderBridge.OR.class),
+	NOT("NOT gate", GateLogic.NOT.class, GateRenderBridge.NOT.class),
+	RSLATCH("RS-Latch", GateLogic.RSLatch.class, GateRenderBridge.RSLatch.class),
+	TOGGLE("T-FlipFlop", GateLogic.ToggleLatch.class, GateRenderBridge.ToggleLatch.class),
+	NOR("NOR gate", GateLogic.NOR.class, GateRenderBridge.NOR.class),
+	NAND("NAND gate", GateLogic.NAND.class, GateRenderBridge.NAND.class),
+	XOR("XOR gate", GateLogic.XOR.class, GateRenderBridge.XOR.class),
+	XNOR("XNOR gate", GateLogic.XNOR.class, GateRenderBridge.XNOR.class),
+	Buffer("Buffer gate", GateLogic.Buffer.class, GateRenderBridge.Buffer.class),
+	Multiplexer("Multiplexer", GateLogic.Multiplexer.class, GateRenderBridge.Multiplexer.class),
+	Repeater("Repeater", GateLogic.Repeater.class, GateRenderBridge.Repeater.class),
+	Timer("Timer", GateLogic.Timer.class, GateRenderBridge.Timer.class),
+	Counter("Counter", GateLogic.Counter.class, GateRenderBridge.Counter.class),
+	Sequencer("Sequencer", GateLogic.Sequencer.class, GateRenderBridge.Sequencer.class),
+	PulseFormer("Pulse Former", GateLogic.PulseFormer.class, GateRenderBridge.PulseFormer.class),
+	Randomizer("Randomizer", GateLogic.Randomizer.class, GateRenderBridge.Randomizer.class),
+	StateCell("State Cell", GateLogic.StateCell.class, GateRenderBridge.StateCell.class),
+	Synchronizer("Synchronizer", GateLogic.Synchronizer.class, GateRenderBridge.Synchronizer.class),
+	DLatch("D-Latch", GateLogic.DLatch.class, GateRenderBridge.DLatch.class),
+	DFlop("D-FlipFlop", GateLogic.DFlop.class, GateRenderBridge.DFlop.class),
+	BundledLatch("Bundled Latch", GateLogic.BundledLatch.class, GateRenderBridge.BundledLatch.class),
+	BundledRelay("Bundled Relay", GateLogic.BundledRelay.class, GateRenderBridge.BundledRelay.class),
+	BundledMultiplexer("Bundled Multiplexer", GateLogic.BundledMultiplexer.class, GateRenderBridge.BundledMultiplexer.class),
+	LightSensor("Light Sensor", GateLogic.LightSensor.class, GateRenderBridge.LightSensor.class),
+	RainSensor("Rain Sensor", GateLogic.RainSensor.class, GateRenderBridge.RainSensor.class)
 	;
 	
 	private Class<? extends GateLogic> logicClass;
 	private Class<? extends GateRenderBridge> renderClass;
 	private GateLogic logicInst;
 	private GateRenderBridge renderInst;
-	private int lightCount;
 	public final String name;
 	public int meta = this.ordinal();
 	public static final String oreDictDefinition = "projredGate";
 	
-	private EnumGate(String name, int lights, Class<? extends GateLogic> logicClass, Class<? extends GateRenderBridge> renderClass) {
+	private EnumGate(String name, Class<? extends GateLogic> logicClass, Class<? extends GateRenderBridge> renderClass) {
 		this.name = name;
-		this.lightCount = lights;
 		
 		this.logicClass = logicClass;
 		this.renderClass = renderClass;
@@ -69,10 +67,6 @@ public enum EnumGate {
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
-	}
-	
-	public int getLightCount() {
-		return lightCount;
 	}
 	
 	@SideOnly(Side.CLIENT)
