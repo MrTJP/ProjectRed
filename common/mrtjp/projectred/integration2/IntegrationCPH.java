d75b931aeae648b154098b2b324d46eb44cc33c3
MrTJP <mrtjp@icloud.com>
2013-08-29 08:13:59 -0400

    Counters

--- common/mrtjp/projectred/integration2/IntegrationCPH.java
+++ common/mrtjp/projectred/integration2/IntegrationCPH.java
@@ -1,6 +1,8 @@
 package mrtjp.projectred.integration2;
 
+import static mrtjp.projectred.integration2.IntegrationSPH.readPartIndex;
 import mrtjp.projectred.ProjectRedIntegration;
+import mrtjp.projectred.integration2.GateLogic.ICounterGuiLogic;
 import mrtjp.projectred.integration2.GateLogic.ITimerGuiLogic;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.NetClientHandler;
@@ -8,8 +10,6 @@ import net.minecraft.world.World;
 import codechicken.lib.packet.PacketCustom;
 import codechicken.lib.packet.PacketCustom.IClientPacketHandler;
 import codechicken.multipart.TMultiPart;
-
-import static mrtjp.projectred.integration2.IntegrationSPH.*;
 
 public class IntegrationCPH implements IClientPacketHandler {
     public static Object channel = ProjectRedIntegration.instance;
@@ -21,6 +21,9 @@ public class IntegrationCPH implements IClientPacketHandler {
             case 1:
                 openTimerGui(mc, mc.theWorld, packet);
                 break;
+            case 2:
+                openCounterGui(mc, mc.theWorld, packet);
+                break;
         }
     }
 
@@ -30,6 +33,15 @@ public class IntegrationCPH implements IClientPacketHandler {
             GatePart gate = (GatePart)part;
             if(gate.getLogic() instanceof ITimerGuiLogic)
                 mc.displayGuiScreen(new GuiTimer(gate));
+        }
+    }
+    
+    private void openCounterGui(Minecraft mc, World world, PacketCustom packet) {
+        TMultiPart part = readPartIndex(world, packet);
+        if(part instanceof GatePart) {
+            GatePart gate = (GatePart)part;
+            if(gate.getLogic() instanceof ICounterGuiLogic)
+                mc.displayGuiScreen(new GuiCounter(gate));
         }
     }
 }
