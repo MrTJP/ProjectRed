a06a882186afa5cbbe5a9795748672be2976e49b
MrTJP <mrtjp@icloud.com>
2013-12-14 23:09:21 -0500

    Max halo config option

--- common/mrtjp/projectred/core/Configurator.java
+++ common/mrtjp/projectred/core/Configurator.java
@@ -106,7 +106,7 @@ public class Configurator
     public static boolean logicwires3D;
     public static boolean staticWires;
     public static boolean staticGates;
-    public static boolean renderLampHalos;
+    public static int lightHaloMax;
 
     public static void initConfig(FMLPreInitializationEvent event)
     {
@@ -195,7 +195,7 @@ public class Configurator
         staticGates = localConfig.get("general", "renderStaticGates", true, "If set to false, gates will be rendered in the TESR rather than the WorldRenderer").getBoolean(true);
         retroGeneration = localConfig.get("general", "Retro Ore Generation", false, "If set to true, world generation for ProjectRed will attempt to run even in previously generated chunks.").getBoolean(false);
         retroGenID = localConfig.get("general", "Retro Ore Gen ID", "prRG", "This ID is used to check if retro gen has been performed on a chunk. Changing it will reset retro gen status.").getString();
-        renderLampHalos = localConfig.get("general", "Lamp Halo rendering", true, "If set to false, lamps will no longer render halos").getBoolean(true);
+        lightHaloMax = localConfig.get("general", "Light Halo Render Count", true, "Number of lights to render, -1 for unlimited").getInt();
 
         maxDetectionCount = localConfig.get("general", "Max Detection Count", 100, "Max number of links to explore when discovering new routers.").getInt();
         maxDetectionLength = localConfig.get("general", "Max Detection Length", 50, "Max number of links allowed in between routers.").getInt();
