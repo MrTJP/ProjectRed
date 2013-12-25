e07378315a663ab1cae01f0ed40bdb5e18e98078
MrTJP <mrtjp@icloud.com>
2013-12-25 17:03:42 -0500

    Replaced temporary pipe renderer

--- common/mrtjp/projectred/transportation/RenderPipe.java
+++ common/mrtjp/projectred/transportation/RenderPipe.java
@@ -13,6 +13,7 @@ import org.lwjgl.opengl.GL11;
 
 import codechicken.lib.lighting.LazyLightMatrix;
 import codechicken.lib.lighting.LightModel;
+import codechicken.lib.math.MathHelper;
 import codechicken.lib.render.CCModel;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.IUVTransformation;
@@ -69,7 +70,7 @@ public class RenderPipe
         }
     }
 
-    private static class WireFrameModelGenerator
+    private static class PipeModelGenerator
     {
         double w = 2 / 8D;
         double d = 1 / 16D - 0.002;// little offset for compensating for the
@@ -77,8 +78,9 @@ public class RenderPipe
 
         public static void generateModels()
         {
-            WireFrameModelGenerator gen_inst = new WireFrameModelGenerator();
+            PipeModelGenerator gen_inst = new PipeModelGenerator();
             gen_inst.generateCenterModel();
+            gen_inst.generateCrossExclusiveModels();
             gen_inst.generateSideModels();
 
             gen_inst.finishModels();
@@ -88,51 +90,78 @@ public class RenderPipe
         {
             CCModel model = CCModel.quadModel(48);
 
-            model.verts[0] = new Vertex5(0.5 - w, 0.5 - w, 0.5 - w, 20, 8);
-            model.verts[1] = new Vertex5(0.5 + w, 0.5 - w, 0.5 - w, 28, 8);
-            model.verts[2] = new Vertex5(0.5 + w, 0.5 - w, 0.5 + w, 28, 0);
-            model.verts[3] = new Vertex5(0.5 - w, 0.5 - w, 0.5 + w, 20, 0);
+            model.verts[0] = new Vertex5(0.5 - w, 0.5 - w, 0.5 - w, 4, 8);
+            model.verts[1] = new Vertex5(0.5 + w, 0.5 - w, 0.5 - w, 12, 8);
+            model.verts[2] = new Vertex5(0.5 + w, 0.5 - w, 0.5 + w, 12, 0);
+            model.verts[3] = new Vertex5(0.5 - w, 0.5 - w, 0.5 + w, 4, 0);
 
-            model.verts[4] = new Vertex5(0.5 - w, 0.5 - w + d, 0.5 + w, 20, 8);
-            model.verts[5] = new Vertex5(0.5 + w, 0.5 - w + d, 0.5 + w, 28, 8);
-            model.verts[6] = new Vertex5(0.5 + w, 0.5 - w + d, 0.5 - w, 28, 0);
-            model.verts[7] = new Vertex5(0.5 - w, 0.5 - w + d, 0.5 - w, 20, 0);
+            model.verts[4] = new Vertex5(0.5 - w, 0.5 - w + d, 0.5 + w, 4, 8);
+            model.verts[5] = new Vertex5(0.5 + w, 0.5 - w + d, 0.5 + w, 12, 8);
+            model.verts[6] = new Vertex5(0.5 + w, 0.5 - w + d, 0.5 - w, 12, 0);
+            model.verts[7] = new Vertex5(0.5 - w, 0.5 - w + d, 0.5 - w, 4, 0);
 
             model.generateSidedParts(0, Vector3.center);
-            frameModels[6] = model;
+            centerModels[3] = model;
+        }
+        
+        public void generateCrossExclusiveModels()
+        {
+            CCModel model = CCModel.quadModel(48);
+            
+            model.verts[0] = new Vertex5(0.5 - w, 0.5 - w, 0.5 - w, 0, 16);
+            model.verts[1] = new Vertex5(0.5 + w, 0.5 - w, 0.5 - w, 8, 16);
+            model.verts[2] = new Vertex5(0.5 + w, 0.5 - w, 0.5 + w, 8, 8);
+            model.verts[3] = new Vertex5(0.5 - w, 0.5 - w, 0.5 + w, 0, 8);
+
+            model.verts[4] = new Vertex5(0.5 - w, 0.5 - w + d, 0.5 + w, 0, 16);
+            model.verts[5] = new Vertex5(0.5 + w, 0.5 - w + d, 0.5 + w, 8, 16);
+            model.verts[6] = new Vertex5(0.5 + w, 0.5 - w + d, 0.5 - w, 8, 8);
+            model.verts[7] = new Vertex5(0.5 - w, 0.5 - w + d, 0.5 - w, 0, 8);
+
+            for(int s = 1; s < 4; s++)
+                model.generateSidedPart(0, s, Vector3.center, 0, 8*s, 8);
+
+            //TODO ask CB about this
+            for (int i = 0; i < 48; i++)
+                if (model.verts[i] == null)
+                    model.verts[i] = new Vertex5();
+            
+            centerModels[0] = model.copy().apply(Rotation.sideOrientation(2, 1).at(Vector3.center));
+            centerModels[1] = model.copy().apply(Rotation.sideOrientation(0, 1).at(Vector3.center));
+            centerModels[2] = model;
         }
 
         public void generateSideModels()
         {
             CCModel model = CCModel.quadModel(36);
 
-            model.verts[0] = new Vertex5(0.5 - w, 0, 0.5 + w, 16, 0);
-            model.verts[1] = new Vertex5(0.5 + w, 0, 0.5 + w, 16, 8);
-            model.verts[2] = new Vertex5(0.5 + w, 0.5 - w, 0.5 + w, 20, 8);
-            model.verts[3] = new Vertex5(0.5 - w, 0.5 - w, 0.5 + w, 20, 0);
+            model.verts[0] = new Vertex5(0.5 - w, 0, 0.5 + w, 0, 0);
+            model.verts[1] = new Vertex5(0.5 + w, 0, 0.5 + w, 0, 8);
+            model.verts[2] = new Vertex5(0.5 + w, 0.5 - w, 0.5 + w, 4, 8);
+            model.verts[3] = new Vertex5(0.5 - w, 0.5 - w, 0.5 + w, 4, 0);
 
-            model.verts[4] = new Vertex5(0.5 + w, 0, 0.5 + w - d, 16, 0);
-            model.verts[5] = new Vertex5(0.5 - w, 0, 0.5 + w - d, 16, 8);
-            model.verts[6] = new Vertex5(0.5 - w, 0.5 - w, 0.5 + w - d, 20, 8);
-            model.verts[7] = new Vertex5(0.5 + w, 0.5 - w, 0.5 + w - d, 20, 0);
+            model.verts[4] = new Vertex5(0.5 + w, 0, 0.5 + w - d, 0, 0);
+            model.verts[5] = new Vertex5(0.5 - w, 0, 0.5 + w - d, 0, 8);
+            model.verts[6] = new Vertex5(0.5 - w, 0.5 - w, 0.5 + w - d, 4, 8);
+            model.verts[7] = new Vertex5(0.5 + w, 0.5 - w, 0.5 + w - d, 4, 0);
 
             for (int r = 1; r < 4; r++)
                 model.apply(Rotation.quarterRotations[r].at(Vector3.center), 0, r * 8, 8);
 
-            model.verts[32] = new Vertex5(0.5 - w, 0, 0.5 - w, 24, 32);
-            model.verts[33] = new Vertex5(0.5 + w, 0, 0.5 - w, 32, 32);
-            model.verts[34] = new Vertex5(0.5 + w, 0, 0.5 + w, 32, 24);
-            model.verts[35] = new Vertex5(0.5 - w, 0, 0.5 + w, 24, 24);
+            model.verts[32] = new Vertex5(0.5 - w, 0, 0.5 - w, 8, 16);
+            model.verts[33] = new Vertex5(0.5 + w, 0, 0.5 - w, 16, 16);
+            model.verts[34] = new Vertex5(0.5 + w, 0, 0.5 + w, 16, 8);
+            model.verts[35] = new Vertex5(0.5 - w, 0, 0.5 + w, 8, 8);
 
-            frameModels[0] = model;
+            sideModels[0] = model;
             for (int s = 1; s < 6; s++)
             {
-                frameModels[s] = model.copy().apply(Rotation.sideRotations[s].at(Vector3.center));
+                sideModels[s] = model.copy().apply(Rotation.sideRotations[s].at(Vector3.center));
 
                 if (s % 2 == 1)
                 {
-                    Vertex5[] verts = frameModels[s].verts;
-                    UVT t = new UVT(Rotation.quarterRotations[2].at(new Vector3(24, 0, 4)));
+                    Vertex5[] verts = sideModels[s].verts;
+                    UVT t = new UVT(Rotation.quarterRotations[2].at(new Vector3(8, 0, 4)));
                     for (int i = 0; i < 32; i++)
                         verts[i].apply(t);
                 }
@@ -141,14 +170,16 @@ public class RenderPipe
 
         public void finishModels()
         {
-            for (CCModel m : frameModels)
+            for (CCModel m : sideModels)
+                finishModel(m);
+            for (CCModel m : centerModels)
                 finishModel(m);
         }
     }
 
     public static CCModel finishModel(CCModel m)
     {
-        m.apply(new UVScale(1 / 32D));
+        m.apply(new UVScale(1 / 16D));
         m.shrinkUVs(0.0005);
         m.computeNormals();
         m.computeLighting(LightModel.standardLightModel);
@@ -166,13 +197,14 @@ public class RenderPipe
         }
     }
 
-    public static CCModel[] frameModels = new CCModel[7];
+    public static CCModel[] sideModels = new CCModel[6];
+    public static CCModel[] centerModels = new CCModel[4];
 
     private static LazyLightMatrix dynamicLight = new LazyLightMatrix();
 
     static
     {
-        WireFrameModelGenerator.generateModels();
+        PipeModelGenerator.generateModels();
     }
 
     public static void render(BasicPipePart w, Vector3 pos)
@@ -183,19 +215,26 @@ public class RenderPipe
 
     public static void render(BasicPipePart w, Vector3 pos, LazyLightMatrix olm)
     {
-        WireFrameModelGenerator.generateModels();
-        int key = w.connMap | 1 << 6;
         Transformation t = new Translation(pos);
         IUVTransformation uvt = new IconTransformation(w.getIcon(6));
+        
+        int connMap = w.connMap;
 
-        frameModels[6].render(t, uvt);
+        if (Integer.bitCount(connMap) == 2 && ((connMap&3) == 3 || (connMap&12) == 12 || (connMap&48) == 48))
+            for (int a = 0; a < 3; a++)
+            {
+                if (connMap >> a * 2 == 3)
+                    centerModels[a].render(t, uvt);
+            }
+        else
+            centerModels[3].render(t, uvt);     
+        
         for (int s = 0; s < 6; s++)
-            if ((key & 1 << s) != 0)
+            if ((connMap & 1 << s) != 0)
             {
                 uvt = new IconTransformation(w.getIcon(s));
-                frameModels[s].render(t, uvt);
+                sideModels[s].render(t, uvt);
             }
-
     }
 
     public static void renderBreakingOverlay(Icon icon, BasicPipePart wire)
@@ -210,9 +249,9 @@ public class RenderPipe
 
         CCRenderState.setColour(-1);
 
-        frameModels[6].render(t, uvt);
+        centerModels[3].render(t, uvt);
         for (int s = 0; s < 2; s++)
-            frameModels[s].render(t, uvt);
+            sideModels[s].render(t, uvt);
 
     }
 
@@ -232,24 +271,12 @@ public class RenderPipe
             double frameZ = pos.z + 0.5D;
 
             switch (dir) {
-            case UP:
-                frameY = (pos.y - 0.25D) + prog;
-                break;
-            case DOWN:
-                frameY = (pos.y - 0.25D) + (1.0D - prog);
-                break;
-            case SOUTH:
-                frameZ = pos.z + prog;
-                break;
-            case NORTH:
-                frameZ = pos.z + (1.0D - prog);
-                break;
-            case EAST:
-                frameX = pos.x + prog;
-                break;
-            case WEST:
-                frameX = pos.x  + (1.0D - prog);
-                break;
+            case UP: frameY = (pos.y - 0.25D) + prog; break;
+            case DOWN: frameY = (pos.y - 0.25D) + (1.0D - prog); break;
+            case SOUTH: frameZ = pos.z + prog; break;
+            case NORTH: frameZ = pos.z + (1.0D - prog); break;
+            case EAST: frameX = pos.x + prog; break;
+            case WEST: frameX = pos.x  + (1.0D - prog); break;
             default:
             }
 
@@ -276,8 +303,8 @@ public class RenderPipe
         GL11.glEnable(GL11.GL_LIGHTING);
 
         Tessellator.instance.setColorRGBA_I(PRColors.get(r.priority.color).rgb, 32);
-        GL11.glScalef(.5f, .5f, .5f);
-        RenderUtils.renderBlock(Cuboid6.full, 0, new Translation(-.5, -.5, -.5), null, null);
+        GL11.glScalef(0.5f, 0.5f, 0.5f);
+        RenderUtils.renderBlock(Cuboid6.full, 0, new Translation(-0.5, -0.5, -0.5), null, null);
         restoreRenderState();
 
         GL11.glPopMatrix();
