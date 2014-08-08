7aa07babfc5ae25dcf5a5ab0ecf9c82c3e9f4f0b
Mr TJP <mrtjp@icloud.com>
2014-08-08 11:35:22 -0400

    Fixed furnace recipe crash

--- src/mrtjp/projectred/expansion/ExpansionRecipes.scala
+++ src/mrtjp/projectred/expansion/ExpansionRecipes.scala
@@ -1,14 +1,36 @@
 package mrtjp.projectred.expansion
 
+import cpw.mods.fml.common.registry.GameRegistry
+import mrtjp.projectred.ProjectRedExpansion
+import net.minecraft.init.{Items, Blocks}
+import net.minecraft.item.ItemStack
+
+import java.lang.{Character => JC}
+
 object ExpansionRecipes
 {
     def initRecipes()
     {
-        FurnaceRecipeLib.init()
+        //FurnaceRecipeLib.init()
+        initMachineRecipes()
         initMiscRecipes()
     }
 
     private def initMiscRecipes()
     {
+    }
+
+    private def initMachineRecipes()
+    {
+        //Router controller
+        GameRegistry.addRecipe(new ItemStack(ProjectRedExpansion.machine2, 1, 0),
+            "w w",
+            "ioi",
+            "sss",
+            'w':JC, Blocks.planks,
+            'i':JC, Items.iron_ingot,
+            'o':JC, Blocks.obsidian,
+            's':JC, Blocks.cobblestone
+        )
     }
 }
