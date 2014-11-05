30a4869923c283af4347ad4c846c720ebdcced54
MrTJP <mrtjp@icloud.com>
2014-11-04 23:05:51 -0500

    Shifted build number
    - Allow jenkins to start at current build number

--- src/mrtjp/projectred/exploration/ItemToolProxies.java
+++ src/mrtjp/projectred/exploration/ItemToolProxies.java
@@ -6,34 +6,39 @@ import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemSword;
 
 /**
- *  Proxy scala constructor calls to Java. Fixes weird compiler issue
+ * Proxy scala constructor calls to Java. Fixes weird compiler issue
  */
-class ItemToolProxies {
-	static class Axe extends ItemAxe {
+class ItemToolProxies
+{
+    static class Axe extends ItemAxe
+    {
+        protected Axe(net.minecraft.item.Item.ToolMaterial material)
+        {
+            super(material);
+        }
+    }
 
-		protected Axe(net.minecraft.item.Item.ToolMaterial material) {
-			super(material);
-		}
-	}
+    static class Pickaxe extends ItemPickaxe
+    {
+        protected Pickaxe(net.minecraft.item.Item.ToolMaterial material)
+        {
+            super(material);
+        }
+    }
 
-	static class Pickaxe extends ItemPickaxe {
+    static class Shovel extends ItemSpade
+    {
+        protected Shovel(net.minecraft.item.Item.ToolMaterial material)
+        {
+            super(material);
+        }
+    }
 
-		protected Pickaxe(net.minecraft.item.Item.ToolMaterial material) {
-			super(material);
-		}
-	}
-
-	static class Shovel extends ItemSpade {
-
-		protected Shovel(net.minecraft.item.Item.ToolMaterial material) {
-			super(material);
-		}
-	}
-
-	static class Sword extends ItemSword {
-
-		protected Sword(net.minecraft.item.Item.ToolMaterial material) {
-			super(material);
-		}
-	}
+    static class Sword extends ItemSword
+    {
+        protected Sword(net.minecraft.item.Item.ToolMaterial material)
+        {
+            super(material);
+        }
+    }
 }
