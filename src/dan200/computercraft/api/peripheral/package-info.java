01138b14523b5bee2b114dec5115dc188892ff09
MrTJP <mrtjp@icloud.com>
2014-11-26 11:19:36 -0500

    Updated CC Api
    - Fixes crash with OpenPeripherals

--- src/dan200/computercraft/api/peripheral/package-info.java
+++ src/dan200/computercraft/api/peripheral/package-info.java
@@ -4,7 +4,7 @@
  * For help using the API, and posting your mods, visit the forums at computercraft.info.
  */
 
-@API( owner="ComputerCraft", provides="ComputerCraft|API|Peripheral", apiVersion="1.64pr6" )
+@API( owner="ComputerCraft", provides="ComputerCraft|API|Peripheral", apiVersion="1.65" )
 package dan200.computercraft.api.peripheral;
 
 import cpw.mods.fml.common.API;
