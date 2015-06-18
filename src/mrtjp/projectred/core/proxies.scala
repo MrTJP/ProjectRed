2eedc983fb0ce1a2995112231387e1d77805e884
MrTJP <mrtjp@icloud.com>
2015-06-17 22:21:37 -0400

    Removed unimplemented import

--- src/mrtjp/projectred/core/proxies.scala
+++ src/mrtjp/projectred/core/proxies.scala
@@ -3,7 +3,6 @@ package mrtjp.projectred.core
 import codechicken.lib.packet.PacketCustom
 import cpw.mods.fml.common.FMLCommonHandler
 import cpw.mods.fml.relauncher.{Side, SideOnly}
-import mrtjp.core.fx.FXEngine
 import mrtjp.projectred.ProjectRedCore._
 import mrtjp.projectred.core.libmc.fx.{ParticleIconRegistry, ParticleManagement}
 import mrtjp.projectred.core.libmc.recipe.RecipeLib
@@ -46,7 +45,7 @@ class CoreProxy_client extends CoreProxy_server
         MinecraftForge.EVENT_BUS.register(ParticleIconRegistry.instance)
         MinecraftForge.EVENT_BUS.register(RenderHalo)
 
-        FXEngine.register()
+        //FXEngine.register()
 
         new PRUpdateChecker
     }
