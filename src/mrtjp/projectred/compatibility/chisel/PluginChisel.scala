fa45eafa5a384ffe18e5d8250eb47f89f33548d8
MrTJP <mrtjp@icloud.com>
2019-07-18 08:21:02 -0700

    Fix Chisel integration
    Fixes #1495
    
    Signed-off-by: MrTJP <mrtjp@icloud.com>

--- src/mrtjp/projectred/compatibility/chisel/PluginChisel.scala
+++ src/mrtjp/projectred/compatibility/chisel/PluginChisel.scala
@@ -3,12 +3,21 @@ package mrtjp.projectred.compatibility.chisel
 import mrtjp.projectred.compatibility.IPRPlugin
 import mrtjp.projectred.core.Configurator
 import mrtjp.projectred.exploration.DecorativeStoneDefs
+import net.minecraft.block.Block
+import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraftforge.fml.common.event.FMLInterModComms
 
 object PluginChisel extends IPRPlugin
 {
-    override def getModIDs = Array("chisel", "projectred-exploration")
+    private val chiselModID = "chisel"
+    private val IMCKeyAddVariation = "add_variation"
+    private val tagKeyGroup = "group"
+    private val tagKeyStack = "stack"
+    private val tagKeyBlock = "block"
+    private val tagKeyMeta = "meta"
+    
+    override def getModIDs = Array(chiselModID, "projectred-exploration")
 
     override def isEnabled = Configurator.compat_Chisel
 
@@ -16,15 +25,31 @@ object PluginChisel extends IPRPlugin
 
     override def init()
     {
-        for (s <- DecorativeStoneDefs.values) {
-            val message = new NBTTagCompound
-            message.setString("group", "projectred")
-            message.setTag("stack", s.makeStack.serializeNBT)
-            FMLInterModComms.sendMessage("chisel", "add_variation", message)
-        }
+        initChiselModIntegration()
     }
 
     override def postInit(){}
 
     override def desc() = "Chisel: Exploration decorative blocks"
+
+    private def initChiselModIntegration()
+    {
+        def addToGroup(group:String, stack:ItemStack, block:Block, meta:Int) {
+            val message = new NBTTagCompound
+            message.setString(tagKeyGroup, group)
+            message.setTag(tagKeyStack, stack.serializeNBT())
+            message.setString(tagKeyBlock, block.getRegistryName.toString)
+            message.setInteger(tagKeyMeta, meta)
+            FMLInterModComms.sendMessage(chiselModID, IMCKeyAddVariation, message)
+        }
+
+        /** Add conversion group [marble, marble brick] **/
+        addToGroup("marble", DecorativeStoneDefs.MARBLE.makeStack, DecorativeStoneDefs.getBlock, DecorativeStoneDefs.MARBLE.meta)
+        addToGroup("marble", DecorativeStoneDefs.MARBLEBRICK.makeStack, DecorativeStoneDefs.getBlock, DecorativeStoneDefs.MARBLEBRICK.meta)
+
+        /** Add conversion group [basalt cobble, basalt brick, basalt stone] **/
+        addToGroup("basalt", DecorativeStoneDefs.BASALTCOBBLE.makeStack, DecorativeStoneDefs.getBlock, DecorativeStoneDefs.BASALTCOBBLE.meta)
+        addToGroup("basalt", DecorativeStoneDefs.BASALT.makeStack, DecorativeStoneDefs.getBlock, DecorativeStoneDefs.BASALT.meta)
+        addToGroup("basalt", DecorativeStoneDefs.BASALTBRICK.makeStack, DecorativeStoneDefs.getBlock, DecorativeStoneDefs.BASALTBRICK.meta)
+    }
 }
