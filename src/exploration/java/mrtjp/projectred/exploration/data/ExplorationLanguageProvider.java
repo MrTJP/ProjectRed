3faf47484e42f398b30c92a8c75513413ad2da3f
MrTJP <mrtjp@icloud.com>
2023-05-12 20:30:35 -0400

    internal: finish moving to generated en_us language files

--- src/exploration/java/mrtjp/projectred/exploration/data/ExplorationLanguageProvider.java
+++ src/exploration/java/mrtjp/projectred/exploration/data/ExplorationLanguageProvider.java
@@ -14,11 +14,14 @@ public class ExplorationLanguageProvider extends LanguageProvider {
 
     @Override
     public String getName() {
-        return "ProjectRed-Exploration Language Provider";
+        return "ProjectRed-Exploration Language Provider: en_us";
     }
 
     @Override
     protected void addTranslations() {
+
+        // Creative tab
+        add("itemGroup." + MOD_ID, "Project Red: Exploration");
 
         // Ores
         add(RUBY_ORE_BLOCK, "Ruby Ore");
@@ -56,5 +59,88 @@ public class ExplorationLanguageProvider extends LanguageProvider {
         add(TIN_BLOCK_WALL, "Tin Wall");
         add(SILVER_BLOCK_WALL, "Silver Wall");
         add(ELECTROTINE_BLOCK_WALL, "Electrotine Wall");
+
+        // Items
+        add(WOOL_GIN, "Wool Gin");
+        add(ATHAME, "Athame");
+
+        // Axes
+        add(RUBY_AXE, "Ruby Axe");
+        add(SAPPHIRE_AXE, "Sapphire Axe");
+        add(PERIDOT_AXE, "Peridot Axe");
+
+        // Pickaxes
+        add(RUBY_PICKAXE, "Ruby Pickaxe");
+        add(SAPPHIRE_PICKAXE, "Sapphire Pickaxe");
+        add(PERIDOT_PICKAXE, "Peridot Pickaxe");
+
+        // Shovels
+        add(RUBY_SHOVEL, "Ruby Shovel");
+        add(SAPPHIRE_SHOVEL, "Sapphire Shovel");
+        add(PERIDOT_SHOVEL, "Peridot Shovel");
+
+        // Hoes
+        add(RUBY_HOE, "Ruby Hoe");
+        add(SAPPHIRE_HOE, "Sapphire Hoe");
+        add(PERIDOT_HOE, "Peridot Hoe");
+
+        // Swords
+        add(RUBY_SWORD, "Ruby Sword");
+        add(SAPPHIRE_SWORD, "Sapphire Sword");
+        add(PERIDOT_SWORD, "Peridot Sword");
+
+        // Helmets
+        add(RUBY_HELMET, "Ruby Helmet");
+        add(SAPPHIRE_HELMET, "Sapphire Helmet");
+        add(PERIDOT_HELMET, "Peridot Helmet");
+
+        // Chestplates
+        add(RUBY_CHESTPLATE, "Ruby Chestplate");
+        add(SAPPHIRE_CHESTPLATE, "Sapphire Chestplate");
+        add(PERIDOT_CHESTPLATE, "Peridot Chestplate");
+
+        // Leggings
+        add(RUBY_LEGGINGS, "Ruby Leggings");
+        add(SAPPHIRE_LEGGINGS, "Sapphire Leggings");
+        add(PERIDOT_LEGGINGS, "Peridot Leggings");
+
+        // Boots
+        add(RUBY_BOOTS, "Ruby Boots");
+        add(SAPPHIRE_BOOTS, "Sapphire Boots");
+        add(PERIDOT_BOOTS, "Peridot Boots");
+
+        // Saws
+        add(GOLD_SAW, "Gold Saw");
+        add(RUBY_SAW, "Ruby Saw");
+        add(SAPPHIRE_SAW, "Sapphire Saw");
+        add(PERIDOT_SAW, "Peridot Saw");
+
+        // Sickles
+        add(WOOD_SICKLE, "Wood Sickle");
+        add(STONE_SICKLE, "Stone Sickle");
+        add(IRON_SICKLE, "Iron Sickle");
+        add(GOLD_SICKLE, "Gold Sickle");
+        add(DIAMOND_SICKLE, "Diamond Sickle");
+        add(RUBY_SICKLE, "Ruby Sickle");
+        add(SAPPHIRE_SICKLE, "Sapphire Sickle");
+        add(PERIDOT_SICKLE, "Peridot Sickle");
+
+        // Backpacks
+        add(WHITE_BACKPACK, "White Backpack");
+        add(ORANGE_BACKPACK, "Orange Backpack");
+        add(MAGENTA_BACKPACK, "Magenta Backpack");
+        add(LIGHT_BLUE_BACKPACK, "Light Blue Backpack");
+        add(YELLOW_BACKPACK, "Yellow Backpack");
+        add(LIME_BACKPACK, "Lime Backpack");
+        add(PINK_BACKPACK, "Pink Backpack");
+        add(GRAY_BACKPACK, "Gray Backpack");
+        add(LIGHT_GRAY_BACKPACK, "Light Gray Backpack");
+        add(CYAN_BACKPACK, "Cyan Backpack");
+        add(PURPLE_BACKPACK, "Purple Backpack");
+        add(BLUE_BACKPACK, "Blue Backpack");
+        add(BROWN_BACKPACK, "Brown Backpack");
+        add(GREEN_BACKPACK, "Green Backpack");
+        add(RED_BACKPACK, "Red Backpack");
+        add(BLACK_BACKPACK, "Black Backpack");
     }
 }
