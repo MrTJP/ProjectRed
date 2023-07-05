a205e2414100c9c7f3b3f2d44d8daa00949d6191
MrTJP <mrtjp@icloud.com>
2023-07-05 18:57:48 -0400

    fix: electrotine generator not breakable with pickaxe

---
+++ core/src/main/java/mrtjp/projectred/core/data/CoreBlockTagsProvider.java
@@ -0,0 +1,31 @@
+package mrtjp.projectred.core.data;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import static mrtjp.projectred.core.ProjectRedCore.MOD_ID;
+
+import static mrtjp.projectred.core.init.CoreReferences.*;
+
+public class CoreBlockTagsProvider extends BlockTagsProvider {
+
+    public CoreBlockTagsProvider(DataGenerator gen, @Nullable ExistingFileHelper existingFileHelper) {
+        super(gen, MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    public String getName() {
+        return "ProjectRed-Core Block Tags";
+    }
+
+    @Override
+    protected void addTags() {
+
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ELECTROTINE_GENERATOR_BLOCK);
+
+    }
+}
