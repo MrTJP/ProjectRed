af52d3dcc31327daec85987a600af6ca369344fd
MrTJP <mrtjp@icloud.com>
2025-12-03 13:57:07 -0500

    fix: frames not checking player collision on conversion placement

--- expansion/src/main/java/mrtjp/projectred/expansion/part/FramePartConverter.java
+++ expansion/src/main/java/mrtjp/projectred/expansion/part/FramePartConverter.java
@@ -5,8 +5,11 @@ import codechicken.multipart.api.part.MultiPart;
 import codechicken.multipart.util.MultipartPlaceContext;
 import mrtjp.projectred.expansion.init.ExpansionBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -29,8 +32,19 @@ public class FramePartConverter extends PartConverter {
     @Override
     public ConversionResult<MultiPart> convert(MultipartPlaceContext context) {
         if (context.getItemInHand().getItem() == ExpansionBlocks.FRAME_BLOCK.get().asItem()) {
-            return ConversionResult.success(new FramePart());
+            BlockState state = ExpansionBlocks.FRAME_BLOCK.get().getStateForPlacement(context);
+            if (state != null && canPlace(context, state)) {
+                return ConversionResult.success(new FramePart());
+            }
         }
         return emptyResult();
+    }
+
+    // Lifted from BlockItem
+    private boolean canPlace(BlockPlaceContext context, BlockState state) {
+        Player player = context.getPlayer();
+        CollisionContext collisioncontext = player == null ? CollisionContext.empty() : CollisionContext.of(player);
+        return (state.canSurvive(context.getLevel(), context.getClickedPos()))
+                && context.getLevel().isUnobstructed(state, context.getClickedPos(), collisioncontext);
     }
 }
